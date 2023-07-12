0cdd65381d3553d520a6d3ab884de4679c4ea94a
Brijeshp18 <brijeshbri18@gmail.com>
2023-07-12 20:50:09 +0530

    web

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Igallery from './components/Igallery'
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Igallery/>
-    </div>
+    <>
+    <Router>
+
+      <Routes>
+
+        <Route path='/' element={<Igallery/>}/>
+        <Route path='/image-card' element={<Igallery/>}/>
+      </Routes>
+    </Router>
+    
+      </>
+   
   )
 }
 
